fcbf7a314bc52d83acaf64b45951836101d56298
Ryan <ryan.jackson.2009@gmail.com>
2026-04-04 11:02:31 -1000

    Reduce featured exhibitions card size on homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -97,7 +97,7 @@ export default async function HomePage() {
                 {t('seeAllExhibitions')} →
               </Link>
             </div>
-            <div className="grid md:grid-cols-2 gap-6">
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
               {exhibitions.map((exhibition) => (
                 <ExhibitionCard
                   key={exhibition.id}
